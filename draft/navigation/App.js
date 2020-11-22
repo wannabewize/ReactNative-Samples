e79c7b0638d8ed75d211e7fac4354a9ecace9715
wannabewize <wannabewize@gmail.com>
2020-11-23 03:32:27 +0900

    stack navigation example 업데이트

--- draft/navigation/App.js
+++ draft/navigation/App.js
@@ -6,109 +6,95 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    Button,
+    View,
+    Text,
+    TextInput
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const HomeScreen = ({navigation}) => {
+    let [text, setText] = useState('');
+    return (
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text style={{fontSize: 20}}>Home Screen</Text>
+            <TextInput
+                style={{borderWidth:1, borderColor:'gray', width: '80%', height: 26, marginVertical: 12}}
+                onChangeText={text => {setText(text)}}
+            />
+            <Button
+                onPress={() => { navigation.navigate('Detail', {value: text})}}
+                title="디테일 보기 - 1"/>
+            <Button
+                onPress={() => { navigation.navigate('Detail2', {value: text})}}
+                title="디테일 보기 - 2"/>
+        </View>
+    )
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const DetailScreen = ({route, navigation}) => {
+    useEffect(() => {
+        console.log('Detail Screen appear!');
+        return () => {
+            console.log('Detail Screen disappear!')
+        }
+    }, [navigation])
+    return (
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text style={{fontSize:20}}>Detail Screen</Text>
+            <Text style={{fontSize: 16, marginVertical: 8}}>Value from Home : {route.params.value}</Text>
+            <Button
+                onPress={() => { navigation.goBack() }}
+                title="뒤로 이동" />
+        </View>
+    )
+}
+
+class DetailScreen2 extends React.Component {
+    componentDidMount() {
+        console.log('Detail Screen2 appear');
+    }
+
+    componentWillUnmount() {
+        console.log('Detail Screen2 disappear');
+    }
+    
+    render() {
+        return (
+            <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+                <Text style={{fontSize:20}}>Detail Screen2</Text>
+                <Text style={{fontSize: 16, marginVertical: 8}}>Value from Home : {this.props.route.params.value}</Text>
+                <Button
+                    onPress={() => { this.props.navigation.goBack() }}
+                    title="뒤로 이동" />
+            </View> 
+        )       
+    }
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+       <NavigationContainer>
+           <Stack.Navigator>
+               <Stack.Screen name="Home" component={HomeScreen} />
+               <Stack.Screen name="Detail"
+                    component={DetailScreen}
+                    options={{
+                        headerRight: (props) => (
+                            <Button
+                                onPress={() => {console.log('reload!')}}
+                                title="Reload"/>
+                        )
+                    }} />
+               <Stack.Screen name="Detail2" component={DetailScreen2} />               
+           </Stack.Navigator>
+       </NavigationContainer>
+    );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
