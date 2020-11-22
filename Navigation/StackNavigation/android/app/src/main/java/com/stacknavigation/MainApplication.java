1e0b34cb7b991b3b1d94543a349e5cd4ec0778a2
wannabewize <wannabewize@gmail.com>
2020-11-23 03:47:01 +0900

    스택 내비게이션 예제

--- Navigation/StackNavigation/android/app/src/main/java/com/stacknavigation/MainApplication.java
+++ Navigation/StackNavigation/android/app/src/main/java/com/stacknavigation/MainApplication.java
@@ -1,4 +1,4 @@
-package com.navigation;
+package com.stacknavigation;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.navigation.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.stacknavigation.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
