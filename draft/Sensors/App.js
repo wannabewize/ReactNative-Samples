98572886f95d22d1b908d050835accf2befd6f7c
wannabewize <wannabewize@gmail.com>
2020-12-14 19:21:03 +0900

    센서 예제

--- draft/Sensors/App.js
+++ draft/Sensors/App.js
@@ -22,10 +22,13 @@ import {
     setUpdateIntervalForType,
     SensorTypes
   } from "react-native-sensors";
+import Geolocation from '@react-native-community/geolocation';
 
 const App = () => {
     let [accelemeterSubscription, setAccelemeterSubscription] = useState(null);
     let [sensorResult, setSensorResult] = useState(null);
+    let [locationWatcher, setLocationWatcher] = useState(null);
+
     sensorAccelerometer = () => {
         if ( accelemeterSubscription ) {
             accelemeterSubscription.unsubscribe();
@@ -38,6 +41,27 @@ const App = () => {
             setAccelemeterSubscription(subscription);
         }
     }
+
+    whereAmI = () => {
+        Geolocation.getCurrentPosition(position => {
+            const info = JSON.stringify(position);
+            setSensorResult(info);            
+        });
+    }
+
+    whereAmI2 = () => {
+        if ( locationWatcher ) {
+            Geolocation.clearWatch(locationWatcher);
+            setLocationWatcher(null);
+        }
+        else {
+            const watcherId = Geolocation.watchPosition(position => {
+                const lastPosition = JSON.stringify(position);
+                setSensorResult(lastPosition);
+            });
+            setLocationWatcher(watcherId);
+        }
+    }    
     return (
         <>
             <StatusBar barStyle="dark-content" />
@@ -45,6 +69,14 @@ const App = () => {
                 <Button
                     title="가속도 측정"
                     onPress={sensorAccelerometer}
+                />
+                <Button
+                    title="위치 측정(한번)"
+                    onPress={whereAmI}
+                />
+                <Button
+                    title="위치 측정(계속)"
+                    onPress={whereAmI2}
                 />
                 <Text>{sensorResult}</Text>
             </SafeAreaView>
