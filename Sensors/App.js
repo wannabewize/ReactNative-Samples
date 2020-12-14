ddf2eb5d874d8eac600a8183f9c3af8b0c030be0
wannabewize <wannabewize@gmail.com>
2020-12-14 23:31:53 +0900

    안드로이드 위치 정보 요청 코드 추가

--- Sensors/App.js
+++ Sensors/App.js
@@ -15,6 +15,9 @@ import {
     Text,
     StatusBar,
     Button,
+    PermissionStatus,
+    PermissionsAndroid,
+    Platform,
 } from 'react-native';
 import {
     accelerometer,
@@ -29,7 +32,7 @@ const App = () => {
     let [sensorResult, setSensorResult] = useState(null);
     let [locationWatcher, setLocationWatcher] = useState(null);
 
-    sensorAccelerometer = () => {
+    const sensorAccelerometer = () => {
         if ( accelemeterSubscription ) {
             accelemeterSubscription.unsubscribe();
             setAccelemeterSubscription(null);
@@ -41,36 +44,94 @@ const App = () => {
             setAccelemeterSubscription(subscription);
         }
     }
-
-    whereAmI = () => {
+    
+    const showCurrentLocation = () => {
         Geolocation.getCurrentPosition(position => {
             const info = JSON.stringify(position);
             setSensorResult(info);            
-        });
+        });        
     }
 
-    whereAmI2 = () => {
+    const whereAmI = () => {
+        setSensorResult(null);
+        if ( Platform.OS == 'android' ) {
+            PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                { title: '위치 정보 접근 요청', message: '위치 정보가 필요합니다.', buttonPositive: '승인', buttonNegative: '기각'}
+            ).then( status => {
+                switch ( status ) {
+                    case PermissionsAndroid.RESULTS.GRANTED:
+                        showCurrentPosition();
+                        return;
+                    case PermissionsAndroid.RESULTS.DENIED:
+                        alert('위치 정보 접근 불가');
+                        return;
+                    case PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN:
+                        console.log('never ask agin');
+                        return;
+                }
+            }).catch ( err => {
+                console.error(err);
+            });
+        } else {
+            showCurrentLocation();
+        }
+    }
+
+    const trackCurrentLocation = () => {
         if ( locationWatcher ) {
+            console.log('gps 센서 중지');
             Geolocation.clearWatch(locationWatcher);
             setLocationWatcher(null);
         }
         else {
+            console.log('gps 센서 시작');
             const watcherId = Geolocation.watchPosition(position => {
                 const lastPosition = JSON.stringify(position);
                 setSensorResult(lastPosition);
             });
             setLocationWatcher(watcherId);
         }
-    }    
+    }
+    
+
+
+    const whereAmI2 = () => {
+        setSensorResult(null);        
+        if ( Platform.OS == 'android' ) {
+            PermissionsAndroid.request( PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION )
+            .then(status => {
+                switch ( status ) {
+                    case PermissionsAndroid.RESULTS.GRANTED:
+                        trackCurrentLocation();
+                        return;
+                    case PermissionsAndroid.RESULTS.DENIED:
+                        alert('위치 정보 접근 불가');
+                        return;
+                    case PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN:
+                        console.log('NEVER_ASK_AGAIN');
+                        return;
+                }
+            }).catch ( err => {
+                console.error(err);
+            });
+        }
+        else {
+            trackCurrentLocation();
+        }
+    }
+    
+    const buttonStyle = {}
     return (
         <>
             <StatusBar barStyle="dark-content" />
             <SafeAreaView>
                 <Button
+                    style={{marginBottom: 10}}
                     title="가속도 측정"
                     onPress={sensorAccelerometer}
                 />
-                <Button
+                <Button                                        
                     title="위치 측정(한번)"
                     onPress={whereAmI}
                 />
@@ -78,7 +139,7 @@ const App = () => {
                     title="위치 측정(계속)"
                     onPress={whereAmI2}
                 />
-                <Text>{sensorResult}</Text>
+                <Text style={{margin: 20}}>{sensorResult}</Text>
             </SafeAreaView>
         </>
     );
