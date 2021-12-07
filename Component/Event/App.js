6ef8df93965e6f8c79ba49e3eaa4e833a890e1ce
wannabewize <wannabewize@gmail.com>
2021-12-08 03:59:48 +0900

    키보드 다루기 예

--- Component/Event/App.js
+++ Component/Event/App.js
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   Button,
   SafeAreaView,
@@ -24,58 +23,59 @@ import {
 } from 'react-native';
 
 
-const App: () => Node = () => {
+const App = () => {
   const handlePress = (e) => {
     console.log('handle press');
   }
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <KeyboardAvoidingView
         keyboardVerticalOffset={30}
         behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <ScrollView contentInsetAdjustmentBehavior="automatic"
-        scrollEventThrottle={1}
-        onScroll={(e) => console.log('ScrollView.onScroll y :', e.nativeEvent.contentOffset.y)}
-        onScrollEndDrag={() => console.log('Scroll.onScrolleEndDrag')}
-        onScrollBeginDrag={() => console.log('Scroll.onScrollBeginDrag')}>
-        <Text style={styles.head}>터치/클릭 이벤트</Text>
-        <Button title="버튼" onPress={handlePress} />
-        <TouchableHighlight
-          style={styles.basicMargin}
-          activeOpacity={0.1}
-          underlayColor="#DDDDDD"
-          onPress={handlePress}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic"
+          style={{ backgroundColor: 'blue' }}
+          scrollEventThrottle={1}
+          onScroll={(e) => console.log('ScrollView.onScroll y :', e.nativeEvent.contentOffset.y)}
+          onScrollEndDrag={() => console.log('Scroll.onScrolleEndDrag')}
+          onScrollBeginDrag={() => console.log('Scroll.onScrollBeginDrag')}>
+          <Text style={styles.head}>터치/클릭 이벤트</Text>
+          <Button title="버튼" onPress={handlePress} />
+          <TouchableHighlight
+            style={styles.basicMargin}
+            activeOpacity={0.1}
+            underlayColor="#DDDDDD"
+            onPress={handlePress}>
             <Text>TouchableHighlight</Text>
-        </TouchableHighlight>
-        <TouchableOpacity
-          activeOpacity={0.2}
-          style={styles.basicMargin}
-          onPress={handlePress}>
+          </TouchableHighlight>
+          <TouchableOpacity
+            activeOpacity={0.2}
+            style={styles.basicMargin}
+            onPress={handlePress}>
             <Text>TouchableOpacity</Text>
-        </TouchableOpacity>
-        <Pressable
-          style={styles.basicMargin}
-          onPress={handlePress}
-          onPressIn={() => console.log('Press In')}
-          onPressOut={() => console.log('Press Out')}>
-          <Text>Pressable</Text>
-        </Pressable>
-        <Text style={styles.head}>편집 이벤트</Text>
-        <TextInput
-          style={[styles.basicMargin, {borderWidth: 1, height: 30}]}
-          onChangeText={(text) => console.log('onChangeText :', text)}
-          onChange={(e) => console.log('onChange :', e.nativeEvent.text)}
-          onEndEditing={(e) => console.log('onEndEditing TextInputA')}
-          onKeyPress={(e) => console.log('onKeyPress :', e.nativeEvent.key)}
-        />
-        <TextInput
-          style={[styles.basicMargin, {borderWidth: 1, height: 30}]}
-          onEndEditing={e => console.log('onEndEditing TextInputB')}
-        />
-        <TextInput
-          style={[styles.basicMargin, {borderWidth: 1, height: 30, marginTop: 300}]} value="Keyboard Avoiding"/>        
-      </ScrollView>
+          </TouchableOpacity>
+          <Pressable
+            style={styles.basicMargin}
+            onPress={handlePress}
+            onPressIn={() => console.log('Press In')}
+            onPressOut={() => console.log('Press Out')}>
+            <Text>Pressable</Text>
+          </Pressable>
+          <Text style={styles.head}>편집 이벤트</Text>
+          <TextInput
+            style={[styles.basicMargin, { borderWidth: 1, height: 30 }]}
+            onChangeText={(text) => console.log('onChangeText :', text)}
+            onChange={(e) => console.log('onChange :', e.nativeEvent.text)}
+            onEndEditing={(e) => console.log('onEndEditing TextInputA')}
+            onKeyPress={(e) => console.log('onKeyPress :', e.nativeEvent.key)}
+          />
+          <TextInput
+            style={[styles.basicMargin, { borderWidth: 1, height: 30 }]}
+            onEndEditing={e => console.log('onEndEditing TextInputB')}
+          />
+          <TextInput
+            style={[styles.basicMargin, { borderWidth: 1, height: 30, marginTop: 300 }]} value="Keyboard Avoiding" />
+        </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
